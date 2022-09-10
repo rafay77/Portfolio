9b76e4f72292c89e5dd8618e53bc7a81440c3554
Rafay Zulfiqar <mrzulfiqar.ee42ceme@student.nust.edu.pk>
2022-09-11 03:27:27 +0500

    Add files via upload
    Assets folder

---
+++ assets/js/temp.js
@@ -0,0 +1,12 @@
+
+var mainbg=rgb(59, 27, 80);
+var mainborder=#c31e3b;
+
+
+
+
+let serviceboxcolor=document.getElementById('servicebox').backgroundColor;
+document.getElementById('servicebox').addEventListener('mouseenter', function(){
+    document.getElementById('servicebox').style.backgroundColor=array[0];
+    
+})
